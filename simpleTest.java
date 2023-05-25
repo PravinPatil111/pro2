f92e0c6c5491c213015cfdb68e90d90a58b19b92
Patil <pravin.c.patil@capgemini.com>
2023-05-25 17:07:23 +0530

    Simple test committing

---
+++ simpleTest.java
@@ -0,0 +1,31 @@
+package com.testCase;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class simpleTest {
+	
+	@BeforeClass
+	public static void envionmetSetup() {
+		
+		System.out.println("Set the Envoirnment");
+}
+		@Test
+		public void test1() {
+		
+		System.out.println("test");
+		
+	}
+		@Test
+		public void test2() {
+		
+		System.out.println("test2");}
+		@AfterClass
+		
+		public static void envionmetClose() {
+			
+		System.out.println("Closing the Envoirnment");
+		}
+
+	}
